52bd79994242cda36ff843ad0d39ac07595e76ff
Anastasiia Smilka <nastyasmilka@gmail.com>
2023-04-17 11:58:05 +0300

    initial commit

---
+++ app/src/main/java/com/test/minigame/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.test.minigame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
